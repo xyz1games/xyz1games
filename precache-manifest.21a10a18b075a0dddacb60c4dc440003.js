436fa0889a36fff912f95c3c90b1d5c5a37862fb
xyz1games <106764578+xyz1games@users.noreply.github.com>
2022-06-03 00:33:32 +0800

    Add files via upload

---
+++ precache-manifest.21a10a18b075a0dddacb60c4dc440003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3573c96c8eb280fea5",
+    "url": "/css/app.a0dbd43d.css"
+  },
+  {
+    "revision": "3e3573c96c8eb280fea5",
+    "url": "/js/app.4a3ca950.js"
+  },
+  {
+    "revision": "40fbd2d16c201470ad65",
+    "url": "/css/chunk-2a85875a.a4ac7be7.css"
+  },
+  {
+    "revision": "40fbd2d16c201470ad65",
+    "url": "/js/chunk-2a85875a.05319899.js"
+  },
+  {
+    "revision": "ac39be49be6d2cea91ed",
+    "url": "/js/chunk-vendors.ccac457f.js"
+  },
+  {
+    "revision": "5a0961b02b92b69e9990b7110fbc066e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
